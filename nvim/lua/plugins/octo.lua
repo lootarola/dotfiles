1fb5a2d37489f3f2d66768b43fb5a9908a1c12a5
lootarola <lucas.otarola@gmail.com>
2025-06-16 16:07:45 -0400

    feat(nvim): add github octo plugin

---
+++ nvim/lua/plugins/octo.lua
@@ -0,0 +1,17 @@
+return {
+  'pwntester/octo.nvim',
+  requires = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require('octo').setup {
+      enable_builtin = true,
+      default_merge_method = 'rebase',
+      default_delete_branch = true,
+      ssh_aliases = { ['github.com-work'] = 'github.com' }, -- Remember to add work alias for github
+      picker = 'telescope',
+    }
+  end,
+}
