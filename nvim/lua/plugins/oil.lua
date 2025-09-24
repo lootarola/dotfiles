322e49cc0ead6af6b9ee997e1a0284a0bfece2f9
lootarola <lucas.otarola@gmail.com>
2025-09-24 16:07:52 -0300

    feat(nvim): add oil customization

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,7 +1,15 @@
 return {
   {
     'stevearc/oil.nvim',
-    opts = {},
+    opts = {
+      columns = {
+        'icon',
+      },
+      delete_to_trash = false,
+      view_options = {
+        show_hidden = true,
+      },
+    },
     dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
 }
