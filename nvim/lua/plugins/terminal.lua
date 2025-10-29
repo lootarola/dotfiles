43223e21d433305e7fb060836919216ef530a0ac
lotarola_meli <lucas.otarola@mercadolibre.cl>
2025-10-28 22:51:10 -0300

    chore(nvim): update toggle term version

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -1,7 +1,7 @@
 return {
   {
     'akinsho/toggleterm.nvim',
-    version = 'v2.12.0',
+    version = 'v2.13.1',
     opts = {
       size = 30,
       start_in_insert = true,
@@ -11,3 +11,4 @@ return {
     },
   },
 }
+
