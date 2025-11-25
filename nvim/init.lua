85f9e5bd4d073513f42083317d6e00b0535a3a3d
lootarola <lucas.otarola@gmail.com>
2025-11-25 17:37:51 -0300

    feat(nvim): set relative numbers configuration to default

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.showmode = false
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.breakindent = true
